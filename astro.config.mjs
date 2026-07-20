d88a8bdd8df2a431773e49a1ec29ccdfa59a00a1
gbindinazeez <gbindinazeez@gmail.com>
2026-07-20 21:20:58 +0100

    Point site at neopasture.com and serve from domain root

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gbindinazeez.github.io',
-  base: '/neopasture',
+  site: 'https://neopasture.com',
 });
